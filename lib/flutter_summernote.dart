8037f8cc95b84dfb00645e62365a2a6eb9edb04f
Daniel Aguilera <twinber@hotmail.com>
2020-09-24 08:09:40 +0200

    icon size only ios

--- lib/flutter_summernote.dart
+++ lib/flutter_summernote.dart
@@ -265,7 +265,7 @@ class FlutterSummernoteState extends State<FlutterSummernote> {
       toolbar = customToolbar;
     }
 
-    String styleButtons = Platform.isIOS ? '<style>body{ font-size:28px!important}.btn{font-size: 48px!important;}</style>' : '';
+    String styleButtons = Platform.isIOS ? '<style>body{ font-size:32px!important}.btn{font-size: 48px!important;}</style>' : '';
 
     return '''
     <!DOCTYPE html>
